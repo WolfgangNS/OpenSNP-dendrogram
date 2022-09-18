37f98cef44e8d6769fd0ff88223622d4eeec6869
WolfgangNS <WolfgangStark7@gmail.com>
2022-09-18 05:28:20 -0400

    Insert automated download commands to shell script
    I left them commented until I have enough disk space to test them

--- convert.sh
+++ convert.sh
@@ -1,3 +1,14 @@
+#curl --progress-bar -o Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_39/GRCh37_mapping/GRCh37.primary_assembly.genome.fa.gz
+#curl -o --progress-bar opensnp_datadump.2017-12-08.zip https://archive.org/download/opensnp_data_dumps/opensnp_datadump.2017-12-08.zip
+
+#git clone --recurse-submodules https://github.com/samtools/htslib.git
+#git clone https://github.com/samtools/bcftools.git
+#cd bcftools
+# # The following is optional:
+# #   autoheader && autoconf && ./configure --enable-libgsl --enable-perl-filters
+#make
+#make install
+
 for i in *.23andme.txt
 do
   echo "converting $i to .vcf"
@@ -9,6 +20,6 @@ done
 bcftools merge -l list.txt -o merged.vcf
 Rscript "dendrogram.r"
 
-#TO-DO: curl command to download GRCh37 (and possibly unpack)
-#TO-DO: code to convert GRCh37 assembly to .fa.gz
+#TO-DO: test recently added code in comments
+#TO-DO: download each of the 94 samples from OpenSNP (or download from archive.org, extract only those 94)
 #for readme: need in this directory: 23andme files as .txt, human genome assembly
